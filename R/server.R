dcb456a1a2716e0f41eabb3a2e80941704579432
HBossier <bossier.han@gmail.com>
2017-03-15 09:24:25 +0100

    patching bug about stock selection

--- R/server.R
+++ R/server.R
@@ -1,19 +1,20 @@
 server <- function(input, output) {
 
+  # Generate data, based on selection of the stock
   output$stockSelection <- renderUI({
     selectInput("selectedstock", label = 'Stocks', choices = Stocks %>% filter(Market == input$markets) %>% select(Naam), selected = 'ABLYNX')
   })
   
+
   # Basic candle plot 
   output$candlePlot <- renderPlot({
-    
     STOCK <- input$selectedstock
     symbol <- Stocks %>% filter(Naam == STOCK) %>% select(Symbol)
     suffix <- Stocks %>% filter(Naam == STOCK) %>% select(suffix)
     end <- today()
     start <- end - weeks(input$weeks)
     HighLowData <- tq_get(paste0(symbol, ".", suffix), get = "stock.prices", from = " 1990-01-01")
-    HighLowData %>%
+    HighLowData %>% 
       ggplot(aes(x = date, y = close)) +
       geom_candlestick(aes(open = open, close = close, high = high, low = low)) + 
       geom_bbands(aes(high = high, low = low, close = close),
@@ -26,13 +27,8 @@ server <- function(input, output) {
                             HighLowData %>% filter(date > start & date < end) %>% select(high) %>% max()))
   })
   
-  output$plot1 <- renderPlot({
-    ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point() +
-      facet_grid(. ~ cyl) +
-      theme_bw()
-  })
-  
-  
+
+  # Volume 
   output$distPlot <- renderPlot({
     STOCK <- input$selectedstock
     symbol <- Stocks %>% filter(Naam == STOCK) %>% select(Symbol)
@@ -40,6 +36,7 @@ server <- function(input, output) {
     end <- today()
     start <- end - weeks(input$weeks)
     HighLowData <- tq_get(paste0(symbol, ".", suffix), get = "stock.prices", from = " 1990-01-01")
+    
     HighLowData %>% filter(date > start & date < end) %>%
       ggplot(aes(x = date, y = close)) +
       geom_candlestick(aes(open = open, close = close, high = high, low = low)) + 
